82c90900b6b194b6c01cb8d99743693a5a83cd02
iamdevoptrainer <yadavkamal26jan@gmail.com>
2023-04-01 20:20:17 +0530

    Added Leaflet Lib.

--- script.js
+++ script.js
@@ -11,4 +11,28 @@ const inputDuration = document.querySelector(".form__input--duration");
 const inputCadence = document.querySelector(".form__input--cadence");
 const inputElevation = document.querySelector(".form__input--elevation");
 
-console.log("Demo");
+navigator.geolocation.getCurrentPosition(
+  function (position) {
+    const { latitude, longitude } = position.coords;
+    // console.log(latitude, longitude);
+    // console.log(`https://www.google.com/maps/@${latitude},${longitude}`);
+    const coords = [latitude, longitude];
+
+    const map = L.map("map").setView(coords, 14);
+
+    L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
+      attribution:
+        '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+    }).addTo(map);
+
+    L.marker(coords)
+      .addTo(map)
+      .bindPopup("A pretty CSS3 popup.<br> Easily customizable.")
+      .openPopup();
+
+    map.on("click", function (mapEvent) {});
+  },
+  function () {
+    alert("Cound not get your location");
+  }
+);
